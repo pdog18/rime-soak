0adff26091e534db74ff8c8dd7c86afa8f87525d
pdog <pdog@qq.com>
2023-02-24 15:54:29 +0800

    change window title，disable resizewindow，hide menu

--- main.js
+++ main.js
@@ -1,14 +1,16 @@
 // main.js
 
 // Modules to control application life and create native browser window
-const { app, BrowserWindow } = require('electron')
+const { app, BrowserWindow, Menu } = require('electron')
 const path = require('path')
 
 const createWindow = () => {
+  Menu.setApplicationMenu(null)
   // Create the browser window.
   const mainWindow = new BrowserWindow({
     width: 800,
     height: 600,
+    resizable: false,
   })
 
   const isDev = true
