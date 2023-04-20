f24b8112eb69cb1a01f42f81ffc6ba73cc89a851
pdog <pdog@qq.com>
2023-04-20 16:46:58 +0800

    [cleanup] Remove unused assets for GitHub workflow

--- src/pages/skin/CustomSkinPreview.tsx
+++ src/pages/skin/CustomSkinPreview.tsx
@@ -1,4 +1,4 @@
-import { useContext, useState } from "react"
+import { useContext } from "react"
 import { Candidate } from "../../store/CustomSkinStore"
 import CustomSkinContext from "./CustomSkinContext"
 import "./animation.css"
